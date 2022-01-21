1d1d6fa3247d046bcd62714e34a49151c19aa53d
airRnot1106 <yngw.rintaro@gmail.com>
2022-01-21 18:14:43 +0900

    TokenIssuerのinit処理を実装

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 
+import { TokenIssuer } from '../issuer/tokenIssuer';
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
+    TokenIssuer.instance;
     console.log(`Express Server Listen START at port=${port}`);
 });
 
