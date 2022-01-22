2bded4da1e17bf93f456aab7f54867f264c7e3fb
airRnot1106 <yngw.rintaro@gmail.com>
2022-01-22 14:15:13 +0900

    応答処理を実装

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,7 @@
 import Discord from 'discord.js';
+import { CommandFactory } from '../command/commandFactory';
 import { TokenIssuer } from '../issuer/tokenIssuer';
+import { MessageParser } from '../parser/messageParser';
 
 const intents: Discord.IntentsString[] = [
     'GUILDS',
@@ -15,8 +17,22 @@ client.on('ready', async () => {
     console.log("Orgel-Al's screw is wound...");
 });
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
+    const messageParser = new MessageParser(message);
+    const parseRes = messageParser.execute();
+    if (parseRes.status === 400 || !parseRes.body) return;
+    const command = CommandFactory.create(
+        parseRes.body.command,
+        parseRes.body.args
+    );
+    const commandRes = await command.execute();
+    console.log(commandRes);
+    if (commandRes.body?.isReply) {
+        await message.reply(commandRes.body.message);
+    } else if (commandRes.body) {
+        await message.channel.send(commandRes.body?.message);
+    }
 });
 
 client.login(TokenIssuer.instance.tokens.DISCORD_BOT_TOKEN);
