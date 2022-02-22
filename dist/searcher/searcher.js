6668984e892e30d26b7b929d71c6f0dbc078b021
airRnot1106 <yngw.rintaro@gmail.com>
2022-02-22 20:13:37 +0900

    build

--- dist/searcher/searcher.js
+++ dist/searcher/searcher.js
@@ -19,24 +19,29 @@ class Searcher {
         return this._instance;
     }
     async execute(q) {
-        const type = this._urlParser.isUrl(q) ? 'url' : 'keyword';
+        const type = this._urlParser.validateType(q);
         switch (type) {
-            case 'url': {
+            case 'video': {
                 const url = q;
-                if (!this._urlParser.isValidUrl(url))
-                    return {
-                        status: 400,
-                        detail: 'URLが正しくありません！',
-                        body: null,
-                    };
-                return await this.searchByUrl(url);
+                return await this.searchByVideoUrl(url);
             }
-            case 'keyword': {
+            case 'search': {
                 return await this.searchByKeyword(q);
+            }
+            case 'playlist': {
+                const url = q;
+                return await this.searchByPlaylist(url);
+            }
+            default: {
+                return {
+                    status: 400,
+                    detail: 'URLが正しくありません！',
+                    body: null,
+                };
             }
         }
     }
-    async searchByUrl(videoUrl) {
+    async searchByVideoUrl(videoUrl) {
         const res = await play_dl_1.default
             .video_basic_info(videoUrl)
             .then((res) => {
@@ -47,7 +52,7 @@ class Searcher {
             return {
                 status: 200,
                 detail: '',
-                body: { id: id, title: title, author: author, url: url },
+                body: [{ id: id, title: title, author: author, url: url }],
             };
         })
             .catch((error) => {
@@ -88,7 +93,7 @@ class Searcher {
             return {
                 status: 200,
                 detail: '',
-                body: { id: id, title: title, author: author, url: url },
+                body: [{ id: id, title: title, author: author, url: url }],
             };
         })
             .catch((error) => {
@@ -101,6 +106,49 @@ class Searcher {
             return {
                 status: 410,
                 detail: 'この動画は再生できません！',
+                body: null,
+            };
+        });
+        return res;
+    }
+    async searchByPlaylist(url) {
+        const res = await play_dl_1.default
+            .playlist_info(url)
+            .then(async (res) => {
+            const targets = (await res.all_videos()).map((target) => {
+                const { id, title, url } = target;
+                const author = target.channel?.name;
+                if (!id || !title || !author)
+                    throw 410;
+                return {
+                    id: id,
+                    title: title,
+                    author: author,
+                    url: url,
+                };
+            });
+            return {
+                status: 200,
+                detail: '',
+                body: targets,
+            };
+        })
+            .catch((error) => {
+            if (/The playlist does not exist/.test(error))
+                return {
+                    status: 404,
+                    detail: 'プレイリストが存在しません！',
+                    body: null,
+                };
+            if (/unavailable video is hidden/.test(error) || error === 410)
+                return {
+                    status: 410,
+                    detail: 'プレイリストに再生できない動画が含まれています！',
+                    body: null,
+                };
+            return {
+                status: 500,
+                detail: '不明なエラー',
                 body: null,
             };
         });
